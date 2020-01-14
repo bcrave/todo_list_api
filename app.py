5b51dc783a803b3e4d21f5f1cc4cb5c5b56afbd2
bcrave <brennycode@gmail.com>
2020-01-13 17:16:16 -0700

    removed database from git.ignore

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
-from flask_heroki import Heroku
+from flask_heroku import Heroku
 import os
 
 app = Flask(__name__)
